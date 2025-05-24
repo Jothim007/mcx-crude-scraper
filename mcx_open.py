c4d002ba125dee195005d0fb451d2acc866740e6
Jothim007 <jothimanikandanbaskaran@gmail.com>
2025-05-24 16:43:09 +0530

    Update mcx_open.py

--- mcx_open.py
+++ mcx_open.py
@@ -2,13 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
+# Set up headless Chrome
 options = Options()
 options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
+# Start browser
 driver = webdriver.Chrome(options=options)
+
+# Open MCX website
 driver.get("https://www.mcxindia.com")
 time.sleep(5)
-print("MCX website opened")
+
+# Take screenshot
+screenshot_path = "mcx_screenshot.png"
+driver.save_screenshot(screenshot_path)
+print(f"Screenshot saved to {screenshot_path}")
+
 driver.quit()
