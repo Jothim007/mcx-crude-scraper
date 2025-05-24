012b8d858474cc76a27c2e3eb0ffe6034c725489
Jothim007 <jothimanikandanbaskaran@gmail.com>
2025-05-24 16:39:56 +0530

    Update mcx_open.py

--- mcx_open.py
+++ mcx_open.py
@@ -2,23 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
-# Set up headless Chrome
 options = Options()
 options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-# Start the browser
 driver = webdriver.Chrome(options=options)
-
-# Open MCX website
 driver.get("https://www.mcxindia.com")
-
-# Wait for page to load
 time.sleep(5)
-
-# Print confirmation
 print("MCX website opened")
-
-# Quit the browser
 driver.quit()
