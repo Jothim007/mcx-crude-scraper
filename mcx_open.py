8c4444f05ea6e30f355294a48c414602ba904e1a
Jothim007 <jothimanikandanbaskaran@gmail.com>
2025-05-24 17:20:20 +0530

    Update mcx_open.py

--- mcx_open.py
+++ mcx_open.py
@@ -1,4 +1,4 @@
-import undetected_chromedriver as uc
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
@@ -7,12 +7,11 @@ options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = uc.Chrome(options=options)
+driver = webdriver.Chrome(options=options)
 driver.get("https://www.mcxindia.com")
+time.sleep(5)
 
-time.sleep(5)  # Give it time to load
-print("MCX website opened")
-
-# Save screenshot
 driver.save_screenshot("mcx_screenshot.png")
+print("Screenshot saved")
+
 driver.quit()
